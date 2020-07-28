dec5f6470e3c517654ab31df1abe79b71cf9a27a
Daniel da Silva <daniel@developmentseed.org>
2020-07-28 17:10:47 +0100

    Add script to generate flood depth index

---
+++ floods/depths2index.js
@@ -0,0 +1,42 @@
+const fs = require('fs-extra');
+const csv = require('csv');
+
+async function main () {
+  const csvData = await fs.readFile('./depths.csv');
+
+  let index = {};
+
+  csv.parse(csvData, { columns: true }, (err, res) => {
+    res.forEach(r => {
+      if (!index[r.flood]) index[r.flood] = {};
+
+      if (!index[r.flood][r.roadId]) {
+        index[r.flood][r.roadId] = {
+          // 5: 0,
+          // 10: 0,
+          // 20: 0,
+          // 50: 0,
+          // 75: 0,
+          // 100: 0,
+          // 200: 0,
+          // 250: 0,
+          // 500: 0,
+          // 1000: 0
+        };
+      }
+
+      index[r.flood][r.roadId][r.rp] = Number(r.mean);
+    });
+
+    console.log('Results saved to:');
+    console.log('');
+    Object.keys(index).forEach(k => {
+      fs.writeJSONSync(`./depths-${k}.json`, index[k]);
+      console.log(` depths-${k}.json`);
+    })
+    console.log('');
+  });
+
+}
+
+main();
